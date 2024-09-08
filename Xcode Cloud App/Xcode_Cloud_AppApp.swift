a7e28c777a13a7b99cf973658f60d5bd7f97f053
nF <naalmuaither@elm.sa>
2024-09-08 20:26:22 +0300

    Initial Commit

---
+++ Xcode Cloud App/Xcode_Cloud_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Cloud_AppApp.swift
+//  Xcode Cloud App
+//
+//  Created by Nawaf  on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Cloud_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
